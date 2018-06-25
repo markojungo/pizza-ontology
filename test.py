7c2a838d766409926752a0fd564cfedd26765326
Jung <Mark.Jung@fda.gov>
2018-06-25 15:50:57 -0400

    printing classes works

--- test.py
+++ test.py
@@ -1,24 +1,17 @@
-from owlready import get_ontology
+from owlready import get_ontology, onto_path
 from shutil import copyfile
 import os
-
-
-class Ontology():
-    def __init__(self, onto):
-        self._onto = onto
-
-    def get_classes(self):
-        for c in self._onto.classes:
-            print(c)
 
 
 def main():
     path = os.getcwd() + "\\pizza.owl"
     copyfile(path, os.getcwd() + "\\pizza_copy.owl")  # copied file for testing
 
-    owlready_ontology = get_ontology(os.getcwd() + "\\pizza_copy.owl")
-    onto = Ontology(owlready_ontology)
-    onto.get_classes()
+    onto_path.append("C:\\Users\\Mark.Jung\\Documents\\pizza-ontology\\")
+    onto = get_ontology("http://pizza.com/ontologies/pizza_copy.owl").load()
+
+    for onto_class in onto.classes:
+        print(onto_class)
 
 
 if __name__ == '__main__':
